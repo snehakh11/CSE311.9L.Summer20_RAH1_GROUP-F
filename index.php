d229df93e1efadbc0394efbdd8b1762953ba3257
snehakh11 <humayra.sneha@northsouth.edu>
2020-09-21 23:39:39 +0600

    new file added

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Login</title>
+	<link rel="stylesheet" type="text/css" href="css/s1.css">
+</head>
+<body>
+<div id="full">
+	<div id="inner_full">
+		<div id="header"><h2> Blood Bank Management System</h2></div>
+		<div id="body">
+			<br><br><br><br><br>
+			<table align="center">
+				<tr>
+					<td width="200px" height="70px"><b>Enter Username</b></td>
+					<td width="100px" height="70px"><input type="text" name="un" placeholder="Enter Username" style="width: 180px;height: 30px;border-radius : 15px;"></td>
+				</tr>
+				<tr>
+					<td width="200px" height="70px"><b>Enter Password</b></td>
+					<td width="200px" height="70px"><input type="text" name="ps" placeholder="Enter Password" style="width: 180px;height: 30px;border-radius: 15px;"></td>
+				</tr>
+				<tr>
+					<td><input type="submit" name="sub" value="Login" style="width: 100px;height: 40px;border-radius: 7px;"></td>
+			    <tr>
+			</table>
+		</div>
+		<div id="footer"><h4 align="center">Donate Blood Save Lives</h4></div>
+	</div>
+</div>
+</body>
+</html>
